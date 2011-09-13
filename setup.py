da7ed14efa402d07d458911c827c24b147fcc23c
Jeremiah Jordan <jeremiah.jordan@morningstar.com>
2011-09-13 16:35:03 -0500

    Add setup.py so it can be installed.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='zkpy',
+      version='0.1',
+      description='Higher level Zookeeper API for Python',
+      author='luk',
+      url='https://github.com/luk/zkpy',
+      packages=['zkpy'],
+     )
